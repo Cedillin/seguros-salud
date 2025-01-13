2530606c079d7518f05276fdcee6762bae15e161
David <david@Davids-MacBook-Pro.local>
2025-01-13 11:03:30 +0100

    first

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/', function () {
     return redirect()->route('client.portal');
 });
 
+Route::get('/dashboard', function () {
+    return redirect()->route('client.portal');
+})->middleware(['auth'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
